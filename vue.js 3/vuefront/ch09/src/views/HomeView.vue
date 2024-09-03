e314b63f6da034b51b2b48e302c7adff85e8142e
jyj76094458 <164857781+jyj76094458@users.noreply.github.com>
2024-09-04 08:47:22 +0900

    20240904 vue.js
    20240904 vue.js

---
+++ vue.js 3/vuefront/ch09/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script>
+import { mapState } from 'pinia';
+import { useCountOptionStore } from '@/stores/countOption';
+import { useCountSetupStore } from '@/stores/countSetup';
+export default {
+  computed: {
+    // this.num, this.doubleNum에 등록됨
+    ...mapState(useCountOptionStore, ['num', 'doubleNum']),
+    ...mapState(useCountSetupStore, {
+      myNum: 'num',
+      myDouble: 'doubleNum',
+    }),
+  },
+}
+</script>
+<template>
+  <h1>Home View</h1>
+  <h2>num: {{ num }}</h2> <!-- 또는 {{ myNum }}-->
+  <h2>doubleNum: {{  doubleNum }}</h2> <!-- 또는 {{ myDouble }}-->
+</template>
