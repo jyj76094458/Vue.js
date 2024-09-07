57affd0df57ac00d4d1130a4fd0af177a22224a7
jyj76094458 <164857781+jyj76094458@users.noreply.github.com>
2024-09-07 22:58:47 +0900

    20240907 js es6
    20240907 js es6

---
+++ vue.js 3/vuefront/ch12/const1.js
@@ -0,0 +1,2 @@
+const name = "철수";    // 에러 발생: Identifier 'name' has already been declared
+const name = "영희";
---
+++ vue.js 3/vuefront/ch12/const2.js
@@ -0,0 +1,3 @@
+let name = "철수";
+name = "영희";  // 선언된 변수에 값 재할당은 가능
+console.log(name);  // 출력 결과: 영희
