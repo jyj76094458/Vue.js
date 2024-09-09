ec2f3d46932a1a277191af042f4d35878134e898
jyj76094458 <164857781+jyj76094458@users.noreply.github.com>
2024-09-09 20:25:08 +0900

    20240909 js es6
    20240909 js es6

---
+++ vue.js 3/vuefront/ch12/spread1.js
@@ -0,0 +1,4 @@
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const concatenatedArr = [...arr1, ...arr2]; // arr1과 arr2 연결
+console.log(concatenatedArr);   // 출력 결과: [1, 2, 3, 4, 5, 6]
---
+++ vue.js 3/vuefront/ch12/template2.js
@@ -0,0 +1,2 @@
+let str1 = "he"llo";    // 에러 발생
+let str2 = 'wor'ld';    // 에러 발생
